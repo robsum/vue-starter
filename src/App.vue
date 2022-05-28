7f0a158740ee6a42cb021c28bf2d6437d9639255
Robert Sumara <rob93pl@gmail.com>
2022-05-28 14:48:18 +0200

    Milligram dependency added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
-<!--    <h1>Twój e-mail to {{ email }}</h1>-->
-<!--    <div v-if="email.length < 10">Ale masz krótki adres!</div>-->
-<!--    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>-->
-<!--    <div v-else class="red">Twój adres e-mail jest stanowczo za długi.</div>-->
-<!--    <input @keyup.enter="alertMyEmail()" type="email" v-model="email">-->
-<!--    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>-->
     <h1>Witaj w systemie do zapisów na zajęcia</h1>
-    <span v-if="!loggedIn">Zaloguj się e-mailem</span>
-    <input v-if="!loggedIn" type="text" v-model="email">
-    <button v-if="!loggedIn" @click="login()">Wchodzę</button>
-    <div v-if="loggedIn"><h2>Witaj {{ email }}!</h2></div>
-    <div v-if="loggedIn"><h3><a href="#" @click="logout()">Wyloguj</a></h3></div>
+    <div v-if="!loggedIn">
+      <span>Zaloguj się e-mailem</span>
+      <input type="text" v-model="email">
+      <button @click="login()">Wchodzę</button>
+    </div>
+    <div v-if="loggedIn">
+      <h2>Witaj {{ email }}!</h2>
+      <h3><a href="#" @click="logout()">Wyloguj</a></h3>
+    </div>
   </div>
 </template>
 
 <script>
+import "milligram";
 export default {
   data() {
     return {
@@ -24,9 +23,6 @@ export default {
     };
   },
   methods: {
-    // alertMyEmail() {
-    //   alert(this.email);
-    // },
     login() {
       this.loggedIn = true;
     },
@@ -39,10 +35,4 @@ export default {
 </script>
 
 <style>
-.red {
-  color: red;
-}
-a {
-  text-decoration:none;
-}
 </style>
